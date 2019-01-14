e0140923931c88eee06406094aaa3b0dae59aa17
abverst <ab_verst@yandex.by>
2019-01-14 10:14:58 +0300

    main bread fix

--- gulpfile.js
+++ gulpfile.js
@@ -21,38 +21,9 @@ var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
     var cssnext=require('postcss-cssnext');
     var autoprefixer=require('autoprefixer');
     var precss=require('precss');
-   /**
-    * 
-    */
-/*
-//OLD
-gulp.task('less',function(){
-	return gulp.src('./app/preproc/*.less')
-        .pipe(less({ plugins: [autoprefixPlugin] }))
-        .pipe(gulp.dest('./dest/css'))
-        .pipe(browserSync.reload({stream:true}));
-});
+    var cpncat=require('gulp-concat');
 
-
-//OLD
-
-gulp.task('scss',function(){
-	return gulp.src('./app/preproc/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 15 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./dest/css'))
-        .pipe(browserSync.reload({stream:true}));
-});
-*/
-
-
-gulp.task('postcss', () => {
-//    const postcss    = require('gulp-postcss')
-    //const sourcemaps = require('gulp-sourcemaps')
-  var   preproc=[
+    var   preproc=[
  
         precss,
         cssnext({
@@ -61,48 +32,57 @@ gulp.task('postcss', () => {
                     grid: true, 
                     browsers: ['last 50 versions', 'ie 6-8', 'Firefox > 20']  
                 }
-            }/*
-            "browserslist": [
-                "last 1 version",
-                "> 1%",
-                "maintained node versions",
-                "not dead"
-              ]*/
+            }
         }),
 
-          /*  autoprefixer({
-            browsers: ['last 100 versions'],
-            cascade: false
-        })*/];
-    return gulp.src('./app/preproc/*.scss')
-        .pipe(sass().on('error', sass.logError))
-    //  .pipe( sourcemaps.init() )
-      .pipe( postcss(preproc) )
-     // .pipe( sourcemaps.write('.') )
-      .pipe( gulp.dest('./dest/css') )
-      .pipe(browserSync.reload({stream:true}));
-  })
+         ];
+         gulp.task('components', () => {
+            //    const postcss    = require('gulp-postcss')
+                //const sourcemaps = require('gulp-sourcemaps')
+              
+                return gulp.src(['./app/**/*.scss',"!./app/preproc/*.scss"])
+                    .pipe(sass().on('error', sass.logError))
+                //  .pipe( sourcemaps.init() )
+                  .pipe( postcss(preproc) )
+                 // .pipe( sourcemaps.write('.') )
+                  .pipe( gulp.dest('./dest') )
+                  .pipe(browserSync.reload({stream:true}));
+            
+              })
+    gulp.task('globalcss', () => {
+      
+                  
+                    return gulp.src(['./app/preproc/*.scss'])
+                        .pipe(sass().on('error', sass.logError))
+                    //  .pipe( sourcemaps.init() )
+                      .pipe( postcss(preproc) )
+                     // .pipe( sourcemaps.write('.') )
+                      .pipe( gulp.dest('./dest/css') )
+                      .pipe(browserSync.reload({stream:true}));
+                
+                  })
 
 
 
 
 
 gulp.task('pug',function () {
-	return gulp.src('./app/*.pug')
+	return gulp.src('./app/**/*.pug')
 	.pipe(pug({
 		pretty:true
 	}))
 	.pipe(gulp.dest('./dest'));
 });
-gulp.task('serve',['postcss'],function(){
+gulp.task('serve',['globalcss','components'],function(){
     browserSync.init({
         server:"./dest"
     });
  //   gulp.watch("./app/preproc/*.less",['less']);
    // gulp.watch("./app/preproc/*.scss",['scss']);
-    gulp.watch("./app/preproc/*.scss",['postcss']);
-    gulp.watch("./app/*.pug",['pug']).on("change",browserSync.reload);;
-    gulp.watch("./app/parts/*.pug",[]).on("change",browserSync.reload);;
+   gulp.watch("./app/components/**/*.scss",['components']);
+   gulp.watch("./app/preproc/*.scss",['globalcss']);
+    gulp.watch("./app/**/*.pug",['pug'])//.on("change",browserSync.reload);;
+   // gulp.watch("./app/parts/*.pug",[]);//.on("change",browserSync.reload);;
     gulp.watch("./dest/*.html").on("change",browserSync.reload);
 
 });
